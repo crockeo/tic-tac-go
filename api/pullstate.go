1eec94886bbdb2a01d61feaa945d636d3a0a3b32
Crockeo <crockeo@gmail.com>
2014-10-27 13:09:15 -0400

    made the API work in such a way that it had a reference to a global board.

--- api/pullstate.go
+++ api/pullstate.go
@@ -1,10 +1,49 @@
 package api
 
 import (
+	"encoding/json"
+	"github.com/crockeo/tic-tac-go/algorithms"
 	"github.com/zenazn/goji/web"
 	"net/http"
 )
 
-func PullState(c web.C, w http.ResponseWriter, r *http.Request) {
+// A type to more easily be used in the PullState function.
+type pullResponse struct {
+	Over   bool  `json:"over"`
+	Winner int   `json:"winner"`
+	Turn   int   `json:"turn"`
+	State  []int `json:"state"`
+}
 
+// Flattening a board.
+func flatten(board algorithms.Board) []int {
+	ints := make([]int, 9)
+
+	i := 0
+	for row := 0; row < 3; row++ {
+		for col := 0; col < 3; col++ {
+			ints[i] = int(algorithms.BoardPull(algorithms.Position{row, col}, board))
+			i++
+		}
+	}
+
+	return ints
+}
+
+func PullState(board *algorithms.Board) func(web.C, http.ResponseWriter, *http.Request) {
+	return func(c web.C, w http.ResponseWriter, r *http.Request) {
+		enc := json.NewEncoder(w)
+
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		err := enc.Encode(pullResponse{
+			algorithms.IsOver(*board),
+			int(algorithms.FindWinner(*board)),
+			int(algorithms.FindTurn(*board)),
+			flatten(*board),
+		})
+
+		if err != nil {
+			panic(err)
+		}
+	}
 }
--- api/pushmove.go
+++ api/pushmove.go
@@ -1,10 +1,13 @@
 package api
 
 import (
+	"github.com/crockeo/tic-tac-go/algorithms"
 	"github.com/zenazn/goji/web"
 	"net/http"
 )
 
-func PushMove(c web.C, w http.ResponseWriter, r *http.Request) {
+func PushMove(board *algorithms.Board) func(web.C, http.ResponseWriter, *http.Request) {
+	return func(c web.C, w http.ResponseWriter, r *http.Request) {
 
+	}
 }
